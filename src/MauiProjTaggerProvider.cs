a804c0f439b83dc373950ef57b72b156bda4221b
Matt Lacey <matt@mrlacey.co.uk>
2022-06-13 11:06:18 +0100

    v1.5 - Add tagging for Color attributes inside .csproj files

---
+++ src/MauiProjTaggerProvider.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.ComponentModel.Composition;
+
+namespace CsInlineColorViz
+{
+    // THis works for any XML file but the inteded application is within .csproj files (for .NET MAUI apps)
+    [Export(typeof(ITaggerProvider))]
+    [ContentType("Xml")]
+    [TagType(typeof(ColorTag))]
+    internal sealed class MauiProjTaggerProvider : ITaggerProvider
+    {
+        public ITagger<T> CreateTagger<T>(ITextBuffer buffer)
+            where T : ITag
+        {
+            if (buffer == null)
+            {
+                throw new ArgumentNullException(nameof(buffer));
+            }
+
+            return buffer.Properties.GetOrCreateSingletonProperty(() => new MauiProjTagger(buffer)) as ITagger<T>;
+        }
+    }
+}
