5ca86c687490b0d1ad35c9e58f20b18b43320db6
Matt Lacey <matt@mrlacey.co.uk>
2022-11-03 14:16:25 +0000

    Support hex literals
    for #5

---
+++ src/HexIntTaggerProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+
+namespace CsInlineColorViz
+{
+    [Export(typeof(ITaggerProvider))]
+    [ContentType("CSharp")]
+    [TagType(typeof(ColorTag))]
+    internal sealed class HexIntTaggerProvider : ITaggerProvider
+    {
+        public ITagger<T> CreateTagger<T>(ITextBuffer buffer)
+            where T : ITag
+        {
+            if (buffer == null)
+            {
+                throw new ArgumentNullException(nameof(buffer));
+            }
+
+            return buffer.Properties.GetOrCreateSingletonProperty(() => new HexIntTagger(buffer)) as ITagger<T>;
+        }
+    }
+}
