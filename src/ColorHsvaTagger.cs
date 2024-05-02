594a319895bb510a454603e6ce9129c2253be553
Matt Lacey <matt@mrlacey.co.uk>
2024-05-02 10:55:22 +0100

    move ColorHsvaTagger to own file

--- src/ColorHsvaTagger.cs
+++ src/ColorHsvaTagger.cs
@@ -5,13 +5,13 @@ using Microsoft.VisualStudio.Text;
 
 namespace CsInlineColorViz;
 
-internal sealed class ColorHslaTagger : RegexTagger<ColorTag>, ITestableRegexColorTagger
+internal sealed class ColorHsvaTagger : RegexTagger<ColorTag>
 {
-	internal static Regex regularExpression = new(@"(Microsoft.Maui.Graphics.Color.FromHsla\(|Color.FromHsla\()([0-9fFDd., ]{5,})(\))", RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
+	internal static Regex regularExpression = new(@"(Microsoft.Maui.Graphics.Color.FromHsva\(|Color.FromHsva\(|Microsoft.Maui.Graphics.Color.FromHsv\(|Color.FromHsv\()([0-9fFDd., ]{5,})(\))", RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
 
 	public Regex ColorExpression => regularExpression;
 
-	internal ColorHslaTagger(ITextBuffer buffer)
+	internal ColorHsvaTagger(ITextBuffer buffer)
 		: base(buffer, [regularExpression])
 	{
 	}
@@ -26,7 +26,7 @@ internal sealed class ColorHslaTagger : RegexTagger<ColorTag>, ITestableRegexCol
 			// Do this check here rather than as part of the RegEx so don't have to adjust the insertion point for the adornment
 			if (new[] { ' ', ',', '(' }.Contains(precedingChar))
 			{
-				if (ColorHelper.TryGetHslaColor(value, out Color clr))
+				if (ColorHelper.TryGetHsvaColor(value, out Color clr))
 				{
 					return new ColorTag(clr, match, lineNumber, lineStart, PopupType.None);
 				}
