23a6c78abf136927469206844910bdce3454fa4d
Matt Lacey <matt@mrlacey.co.uk>
2025-06-13 13:25:55 +0100

    update manifest ID

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -7,12 +7,13 @@ namespace CsInlineColorViz
 {
     internal sealed partial class Vsix
     {
-        public const string Id = "CsInlineColorViz.6b864116-c092-4b7a-aad8-34239a7bbe70";
+        public const string Id = "CsColorViz.6b864116-c092-4b7a-aad8-34239a7bbe70";
         public const string Name = "C# Inline Color Vizualizer";
         public const string Description = @"See samples of the colors you use within your C# code.  ";
         public const string Language = "en-US";
         public const string Version = "1.20.1";
         public const string Author = "Matt Lacey";
         public const string Tags = "";
+        public const bool IsPreview = false;
     }
 }
